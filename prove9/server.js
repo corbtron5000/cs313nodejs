ba1f60bb4ec231b3c46559a6273f99338d0f2496
Corbin Lenning <corbin@Corbins-MacBook-Pro.local>
2019-06-22 11:22:17 -0600

    will this work

--- prove9/server.js
+++ prove9/server.js
@@ -4,7 +4,7 @@ var app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.set("views", "views");
 app.set("view engine", "ejs");
